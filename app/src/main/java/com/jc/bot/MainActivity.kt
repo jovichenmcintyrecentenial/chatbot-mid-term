4b58286aae3de7096327c2944882d7801a7e7dc9
jovi848 <jovi848.mcintyre@gmail.com>
2023-03-02 00:04:16 -0500

    completed main view layout

--- app/src/main/java/com/jc/bot/MainActivity.kt
+++ app/src/main/java/com/jc/bot/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.jc.bot
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+            // Set the system UI visibility and status bar color
+            window?.decorView?.systemUiVisibility = (SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
+
     }
 }
